0e40c8713a88cef73d6cc2f15a33286a0fee30ca
Samuel Cordeiro <samuel.cordeiro100@gmail.com>
2026-09-04 18:37:10 -0300

    (feat): Add swagger and auth configuration

---
+++ csharp-integrations.api/Program.cs
@@ -0,0 +1,41 @@
+using csharp_integrations.core.Auth;
+using csharp_integrations.core.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+#region Bearer Auth
+builder.Services.AddBearerAuthentication(builder.Configuration);
+builder.Services.AddTransient<TokenService>();
+#endregion Bearer Auth
+
+builder.Services.AddControllers();
+
+// Adding Swagger service
+#region Swagger
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerWithBearerSupport("Csharp Integrations Api", "v1", "Collection of endpoints for the CSharp integration Api.");
+#endregion Swagger
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
